90818e6f0b1608e8489717fb614b6c59fb451f25
Briand Quentin <euphoriique@gmail.com>
2021-02-22 15:22:32 +0100

    Ajout d'une API Tea

---
+++ TeaAPI/Controllers/TeaController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TeaAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TeaController : ControllerBase
+    {
+        private static readonly string[] Teas = {
+            "Tea 1", "Tea 2", "Tea 3", "Tea 4", "Tea 5", "Tea 6",
+        };
+
+        [HttpGet]
+        public ActionResult Get()
+        {
+            var rng = new Random();
+
+            return Ok(Teas[rng.Next(Teas.Length)]);
+        }
+    }
+}
